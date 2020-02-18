a573caf4fe2df0c21c80c54c98258a07151e41d4
Faraz Ahmed <fahmed02@icloud.com>
2020-02-18 14:34:54 -0500

    idoi

--- cellRanger2.sh
+++ cellRanger2.sh
@@ -1,70 +1,103 @@
-#----------------------------------------------------------------------------------------
-# get sample names and sample ids
+#!/bin/sh
 
-for i in */
-do
-	cd $i/outs
-	DIR=`pwd`
-	cat input_samplesheet.csv | cut -d "," -f3 | cut -d "-" -f2 | cut -d "_" -f3 | sed 1,2d >> ../../.ids
-	cat input_samplesheet.csv | cut -d "," -f3 | sed 1,2d >> ../../.names
-	cd ../../
+if [ "$1" = "help" ] || [ -z "$1" ]
+    then
+    echo ""
+    echo "--------------------------------------------------------------------------------------"
+    echo " To run this script, use the following syntax:"
+    echo "   bash" $0 "<auto> or <idoi>"
+    echo "    	auto - autodetects duplicate sampleIDs "
+    echo "--------------------------------------------------------------------------------------"
+    echo ""
+    echo ""
+    echo ""
+    exit 1
 
-done
-
-pwd
-
-
-#----------------------------------------------------------------------------------------
-# samples with  multiple run folders; un-comment this for autodetecting samples
-
-# sort .ids | uniq -d > .idoi  
-
-
-#----------------------------------------------------------------------------------------
-# get fastqs path info 
-
-for i in  */
-do
-	cd $i/outs
-	cat input_samplesheet.csv | cut -d "," -f3 | sed 1,2d | awk '{print "'$i'" "outs/fastq_path/ :" $0}' >> ../../.paths.info
-	cd ../../
-
-done
-
-pwd
-
-wc -l .idoi
-
-
-#----------------------------------------------------------------------------------------
-# cellranger count
-
-readarray sampleIDs < .idoi
-
-for i in "${sampleIDs[@]}"
-do
-
-	ID=$i
-	#echo "SAMPLE_ID = $ID"
-	
-	GREP_PATH=$(grep `echo $i`$ .paths.info | cut -d ":" -f1  | xargs | sed -e 's/ /,/g')
-	#echo "FASTQ_PATH = $GREP_PATH" 
-
-	GREP_NAME=$(grep `echo $i`$ .names | xargs | sed -e 's/ /,/g')
-	#echo  "SAMPLE_NAME = $GREP_NAME"
+elif [[ "$1" = "auto" ]]; then
+	#statements
+		#----------------------------------------------------------------------------------------
+		# get sample names and sample ids
+		for i in */
+		do
+			cd $i/outs
+			DIR=`pwd`
+			cat input_samplesheet.csv | cut -d "," -f3 | cut -d "-" -f2 | cut -d "_" -f3 | sed 1,2d >> ../../.ids
+			cat input_samplesheet.csv | cut -d "," -f3 | sed 1,2d >> ../../.names
+			cd ../../
+		done
+		#----------------------------------------------------------------------------------------
+		# samples with  multiple run folders;
+		sort .ids | uniq -d > idoi
+		#----------------------------------------------------------------------------------------
+		# get fastqs path info
+		for i in  */
+		do
+			cd $i/outs
+			cat input_samplesheet.csv | cut -d "," -f3 | sed 1,2d | awk '{print "'$i'" "outs/fastq_path/ :" $0}' >> ../../.paths.info
+			cd ../../
+		done
+		#----------------------------------------------------------------------------------------
+		# cellranger count
+		readarray sampleIDs < idoi
+		for i in "${sampleIDs[@]}"
+		do
+			ID=$i
+			GREP_PATH=$(grep `echo $i`$ .paths.info | cut -d ":" -f1  | xargs | sed -e 's/ /,/g')
+			GREP_NAME=$(grep `echo $i`$ .names | xargs | sed -e 's/ /,/g')
 
 
-	/programs/cellranger-3.0.2/cellranger count --id=$ID \
-  	--transcriptome=/workdir/singleCellData/10x_reference_files/refdata-cellranger-GRCh38-3.0.0/ \
-  	--fastqs=$GREP_PATH \
-  	--sample=$GREP_NAME \
-  	--localcores 20 --localmem 250
+			echo "SAMPLE_ID = $ID"
+			echo "FASTQ_PATH = $GREP_PATH"
+			echo "SAMPLE_NAME = $GREP_NAME"
+			echo "-----------------------------------"
+
+			/programs/cellranger-3.0.2/cellranger count --id=$ID \
+		  	--transcriptome=/workdir/singleCellData/10x_reference_files/refdata-cellranger-GRCh38-3.0.0/ \
+		  	--fastqs=$GREP_PATH \
+		  	--sample=$GREP_NAME \
+		  	--localcores 20 --localmem 250
+		done
+
+elif [[ "$1" = "idoi" ]]; then
+	#statements
+		#----------------------------------------------------------------------------------------
+		# get sample names and sample ids
+		for i in */
+		do
+			cd $i/outs
+			DIR=`pwd`
+			cat input_samplesheet.csv | cut -d "," -f3 | cut -d "-" -f2 | cut -d "_" -f3 | sed 1,2d >> ../../.ids
+			cat input_samplesheet.csv | cut -d "," -f3 | sed 1,2d >> ../../.names
+			cd ../../
+		done
+		#----------------------------------------------------------------------------------------
+		# get fastqs path info
+		for i in  */
+		do
+			cd $i/outs
+			cat input_samplesheet.csv | cut -d "," -f3 | sed 1,2d | awk '{print "'$i'" "outs/fastq_path/ :" $0}' >> ../../.paths.info
+			cd ../../
+		done
+		#----------------------------------------------------------------------------------------
+		# cellranger count
+		readarray sampleIDs < idoi
+		for i in "${sampleIDs[@]}"
+		do
+			ID=$i
+			GREP_PATH=$(grep `echo $i`$ .paths.info | cut -d ":" -f1  | xargs | sed -e 's/ /,/g')
+			GREP_NAME=$(grep `echo $i`$ .names | xargs | sed -e 's/ /,/g')
 
 
-done
+			echo "SAMPLE_ID = $ID"
+			echo "FASTQ_PATH = $GREP_PATH"
+			echo "SAMPLE_NAME = $GREP_NAME"
+			echo "-----------------------------------"
 
+			/programs/cellranger-3.0.2/cellranger count --id=$ID \
+		  	--transcriptome=/workdir/singleCellData/10x_reference_files/refdata-cellranger-GRCh38-3.0.0/ \
+		  	--fastqs=$GREP_PATH \
+		  	--sample=$GREP_NAME \
+		  	--localcores 20 --localmem 250
+		done
 
-
-
-rm .ids .names .paths.info 	
-
+fi
