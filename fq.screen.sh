e54903ea67ea69bfcb1809283c1139c5426cdb19
faraz <33409348+bixBeta@users.noreply.github.com>
2020-04-27 18:06:58 -0400

    Update fq.screen.sh

--- fq.screen.sh
+++ fq.screen.sh
@@ -5,12 +5,28 @@
 #SBATCH -n 12
 #SBATCH --mem-per-cpu=18000
 
-for i in *.gz
-do
-  fastq_screen --outdir fq.screen_out --conf /home/fa286/bin/scripts/my.fastq.conf $i
-done
 
-cd fq.screen_out
-multiqc -n fq.screen.multiqc.report .
+if [ "$1" = "help" ] || [ -z "$1" ]
+then
+    echo ""
+    echo "--------------------------------------------------------------------------------------"
+    echo " To run this script, use the following syntax:"
+    echo "  sbatch "$0"
+    echo "--------------------------------------------------------------------------------------"
+    echo ""
+    echo ""
+    echo ""
+    exit 1
 
-cd ..
+else
+        for i in *.gz
+        do
+          fastq_screen --outdir fq.screen_out --conf /home/fa286/bin/scripts/my.fastq.conf $i
+        done
+
+        cd fq.screen_out
+        multiqc -n fq.screen.multiqc.report .
+
+        cd ..
+fi
+
