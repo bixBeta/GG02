d8a900b9b440d23cb3181f855e9a2961c70f83f9
bixBeta <fahmed02@icloud.com>
2020-11-03 14:05:36 -0500

    Update getMatrix.sh

--- getMatrix.sh
+++ getMatrix.sh
@@ -4,7 +4,6 @@ if [ "$1" = "help" ] || [ -z "$1" ]
     echo "--------------------------------------------------------------------------------------"
     echo " To run this script, use the following syntax:"
     echo "   bash" $0 "<PIN>"
-    echo "    where 1 is for first strand and 2 is for reverse strands, 0 for unstranded counts"
     echo "--------------------------------------------------------------------------------------"
     echo ""
     echo ""
@@ -12,12 +11,11 @@ if [ "$1" = "help" ] || [ -z "$1" ]
     exit 1
 
 
-else 
-    
+else
+
     echo *.ReadsPerGene.out.tab.rawCounts | sed -e 's/ /    /g' > colnames
     paste *.ReadsPerGene.out.tab.rawCounts > $1.rawCounts.txt
 
     cat colnames $1.rawCounts.txt
 
 fi
-
