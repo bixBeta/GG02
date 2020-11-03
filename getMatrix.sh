6055c228ca97fe62a4ab672815a804ce60b9b4b1
Faraz Ahmed <fahmed02@icloud.com>
2020-11-03 14:20:02 -0500

    sadiofhoasifhsao

--- getMatrix.sh
+++ getMatrix.sh
@@ -16,10 +16,7 @@ else
     echo *.ReadsPerGene.out.tab.rawCounts | sed -e 's/ /    /g' > colnames
     paste *.ReadsPerGene.out.tab.rawCounts > $1.rawCounts.txt.temp
 
-<<<<<<< HEAD
     cat colnames $1.rawCounts.txt > $1.rawCounts.txt
-=======
     cat colnames $1.rawCounts.txt.temp > $1.rawCounts.txt
->>>>>>> 74060fac230d814614cbe5dfa4ef795acf9da549
 
 fi
