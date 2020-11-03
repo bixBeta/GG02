74060fac230d814614cbe5dfa4ef795acf9da549
bixBeta <fahmed02@icloud.com>
2020-11-03 14:09:30 -0500

    Update getMatrix.sh

--- getMatrix.sh
+++ getMatrix.sh
@@ -16,6 +16,6 @@ else
 	echo *.ReadsPerGene.out.tab.rawCounts | sed -e 's/ /    /g' > colnames
 	paste *.ReadsPerGene.out.tab.rawCounts > $1.rawCounts.txt.temp
 
-	cat colnames $1.rawCounts.txt > $1.rawCounts.txt
+	cat colnames $1.rawCounts.txt.temp > $1.rawCounts.txt
 
 fi
